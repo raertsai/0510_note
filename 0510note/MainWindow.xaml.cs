6ca7b4ccd15e8e418da931653c2f54c2434a063c
ASUS\USER <raer1332@gmail.com>
2018-05-29 23:53:08 +0800

    no message

--- 0510note/MainWindow.xaml.cs
+++ 0510note/MainWindow.xaml.cs
@@ -24,37 +24,83 @@ namespace _0510note
         {
             InitializeComponent();
         }
-
+          // 存檔文件
         private void save_button_Click(object sender, RoutedEventArgs e)
         {
-            //產生儲存檔案視窗
+            // 產生儲存檔案視窗
             Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
 
-            //顯示視窗
+            // 顯示視窗
             Nullable<bool> result = dig.ShowDialog();
 
-            //按下save後的反應
+            // 按下save後的反應
             if (result == true)
             {
-                //寫入檔案
+                // 寫入檔案
                 System.IO.File.WriteAllText(@"C:\Users\USER\Desktop\temp.txt", Textarea.Text);
             }         
         }       
 
         private void open_button_Click(object sender, RoutedEventArgs e)
         {
-            //產生儲存檔案視窗
+            // 產生儲存檔案視窗
             Microsoft.Win32.OpenFileDialog dig = new Microsoft.Win32.OpenFileDialog();
 
-            //顯示視窗
+            // 顯示視窗
             Nullable<bool> result = dig.ShowDialog();
 
-            //按下open後的反應
+            // 按下open後的反應
             if (result == true)
             {
-                //寫入檔案
+                // 寫入檔案
                 Textarea.Text = System.IO.File.ReadAllText(@"C:\Users\USER\Desktop\temp.txt");
             }
         }
+
+               private void Save_as_button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+
+            // 顯示儲存視窗
+            Nullable<bool> result = dig.ShowDialog();
+        }
+
+        // 讓字體變小
+        private void small_a_click(object sender, RoutedEventArgs e)
+        {
+            Textarea.FontSize = 15;
+        }
+
+        private void medium_a_click(object sender, RoutedEventArgs e)
+        {
+            Textarea.FontSize = 20;
+        }
+
+        private void big_a_click(object sender, RoutedEventArgs e)
+        {
+            Textarea.FontSize = 25;
+        }
+
+        // 讓畫面反白
+        private void white_click(object sender, RoutedEventArgs e)
+        {
+            Textarea.Background = Brushes.White;
+            Textarea.Foreground = Brushes.Gray;
+            small_a.Background = Brushes.White;
+            medium_a.Foreground = Brushes.Gray;
+            big_a.BorderBrush = Brushes.White;
+        }
+
+        // 讓畫面反黑
+        private void black_click(object sender, RoutedEventArgs e)
+        {
+            Textarea.Background = Brushes.Gray;
+            Textarea.Foreground = Brushes.White;
+            small_a.Background = Brushes.Gray;
+            medium_a.Foreground = Brushes.White;
+            big_a.BorderBrush = Brushes.Gray;
+        }
+
+
     }
 }
